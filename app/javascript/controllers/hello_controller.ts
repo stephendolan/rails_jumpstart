09275d1a8e2aeedead168d9d8a0636d2c6ece392
Stephen Dolan <dolan.stephen1@gmail.com>
2020-05-30 15:58:52 -0400

    Make initial test controller actually work with TypeScript

--- app/javascript/controllers/hello_controller.ts
+++ app/javascript/controllers/hello_controller.ts
@@ -1,13 +1,15 @@
 import { Controller } from "stimulus";
 
-// This controller serves as an example of a TypeScript StimulusJS controller
-// It will work with the "strict" settings, which can be difficult with static targets.
-export default class extends Controller {
-  static targets = ["name"];
-
+class HelloBaseController extends Controller {
   readonly nameTarget!: Element;
   readonly nameTargets!: Element[];
   readonly hasNameTargets!: boolean;
+}
+
+// This controller serves as an example of a TypeScript StimulusJS controller
+// It will work with the "strict" settings, which can be difficult with static targets.
+export default class extends (Controller as typeof HelloBaseController) {
+  static targets = ["name"];
 
   public connect(): void {
     console.log("Hello, from a TypeScript controller!");
