b999a89d45667ff4f8f87f9e069621f563637b6c
Stephen Dolan <dolan.stephen1@gmail.com>
2020-05-31 12:40:19 -0400

    Add PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,13 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: [
+    "./app/**/*.html.erb", // Covers both view files and component files
+    "./app/components/**/*.rb", // In case helper functions define CSS classes for components
+    "./app/javascript/controllers/**/*.ts", // In case Stimulus helpers toggle classes
+    "./app/javascript/packs/application.scss", // To include application css styles
+  ],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
 module.exports = {
   plugins: [
     require("postcss-import"),
@@ -9,4 +19,5 @@ module.exports = {
       stage: 3,
     }),
   ],
+  ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
 };
